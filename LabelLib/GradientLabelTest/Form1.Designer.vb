af52d2934bbdc157c9fef80e9bae0edc62c18e52
David A. Turner <dturn73@gmail.com>
2017-04-04 20:44:52 -0700

    Finished exercise 1 of assignment 4.

---
+++ LabelLib/GradientLabelTest/Form1.Designer.vb
@@ -0,0 +1,56 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GradientLabel1 = New LabelLib.GradientLabel()
+        Me.SuspendLayout()
+        '
+        'GradientLabel1
+        '
+        Me.GradientLabel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GradientLabel1.Font = New System.Drawing.Font("Comic Sans MS", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GradientLabel1.LeftColor = System.Drawing.Color.Yellow
+        Me.GradientLabel1.LinearGradientMode = System.Drawing.Drawing2D.LinearGradientMode.ForwardDiagonal
+        Me.GradientLabel1.Location = New System.Drawing.Point(0, 0)
+        Me.GradientLabel1.Name = "GradientLabel1"
+        Me.GradientLabel1.RightColor = System.Drawing.Color.Blue
+        Me.GradientLabel1.Size = New System.Drawing.Size(284, 261)
+        Me.GradientLabel1.TabIndex = 0
+        Me.GradientLabel1.Text = "Fun Label"
+        Me.GradientLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.GradientLabel1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Fun Label Demo"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GradientLabel1 As LabelLib.GradientLabel
+
+End Class
---
+++ LabelLib/LabelLib/GradientLabel.vb
@@ -0,0 +1,54 @@
+﻿Imports System.Drawing
+Imports System.Drawing.Drawing2D
+Public Class GradientLabel
+    Inherits System.Windows.Forms.Label
+
+    Private _LeftColor As System.Drawing.Color
+    Private _RightColor As System.Drawing.Color
+    Private _LinearGradientMode As LinearGradientMode
+
+    Public Sub New()
+        '* Set initial values
+        _LeftColor = Me.BackColor
+        _RightColor = Me.BackColor
+        _LinearGradientMode = LinearGradientMode.Horizontal
+    End Sub
+    Protected Overrides Sub OnPaintBackground(pevent As Windows.Forms.PaintEventArgs)
+        '* Set the rectangular border
+        Dim oRect As Rectangle = New Rectangle(Me.Left, Me.Top, Me.Width, Me.Height)
+        '* Create the Brush
+        Dim oLinearBrush As New LinearGradientBrush(oRect, _LeftColor, _RightColor, _LinearGradientMode)
+        Try
+            '* Fill the rectangle with a gradient
+            pevent.Graphics.FillRectangle(oLinearBrush, oRect)
+        Finally
+            '* Send it to the garbage collector
+            oLinearBrush.Dispose()
+        End Try
+    End Sub
+
+    Public Property LeftColor() As Color
+        Get
+            Return _LeftColor
+        End Get
+        Set(ByVal Value As Color)
+            _LeftColor = Value
+        End Set
+    End Property
+    Public Property RightColor() As Color
+        Get
+            Return _RightColor
+        End Get
+        Set(ByVal Value As Color)
+            _RightColor = Value
+        End Set
+    End Property
+    Public Property LinearGradientMode() As LinearGradientMode
+        Get
+            Return _LinearGradientMode
+        End Get
+        Set(ByVal Value As LinearGradientMode)
+            _LinearGradientMode = Value
+        End Set
+    End Property
+End Class
